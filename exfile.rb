736e5de7e8bd108ff95c1e9bff947677fcaea5a6
KohTakahashi <k.takahashi85@gmail.com>
2012-05-21 12:09:29 +0900

    add external file practice exfile.rb to the repo

---
+++ exfile.rb
@@ -0,0 +1,8 @@
+data = open("users.txt")
+data.each { |line|
+  line =~ /(\w*)\s*:\s*(\w*)/
+  result = {"name" => $1, "passward" => $2}
+  puts "name = %-8s; passwaord = %s" % [$1, $2]
+}
+
+data.close
